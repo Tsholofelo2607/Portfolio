17b19efbbe91302c6b4da1bff48d01fd5f0f4380
Tsholofelo2607 <tsholofelomotswane773@gmail.com>
2025-12-17 11:21:08 -0200

    Add transaction DTOs

---
+++ PersonalFinance.Api/DTOs/Transactions/CreateTransactionsDto.cs
@@ -0,0 +1,11 @@
+namespace PersonalFinance.Api.DTOs.Transactions;
+
+public class CreateTransactionDto
+{
+    public decimal Amount { get; set; }
+    public string Description { get; set; } = "";
+    public DateTime Date { get; set; }
+
+    public int UserId { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ PersonalFinance.Api/DTOs/Transactions/TransactionResponseDto.cs
@@ -0,0 +1,11 @@
+namespace PersonalFinance.Api.DTOs.Transactions;
+
+public class TransactionResponseDto
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; } = "";
+    public DateTime Date { get; set; }
+
+    public string Category { get; set; } = "";
+}
